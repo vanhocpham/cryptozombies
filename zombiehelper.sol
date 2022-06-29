d6f075cf0336e58af3f8396ad9a2582e6f662e04
hoc-anms <phamvanhoc96@gmail.com>
2022-06-29 14:22:16 +0700

    [FEAT] update function payable modifier

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -4,9 +4,16 @@ import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
 
+  uint levelUpFee = 0.001 ether;
+
   modifier aboveLevel(uint _level, uint _zombieId) {
     require(zombies[_zombieId].level >= _level);
     _;
+  }
+
+  function levelUp(uint _zombieId) external payable {
+    require(msg.value == levelUpFee);
+    zombies[_zombieId].level++;
   }
 
 
